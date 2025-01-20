91152e63790a7c07a6fc4fe1d6f0d5a9a5c8593f
SurajGohel <surajgohe321@gmail.com>
2025-01-20 23:08:49 +0530

    Shoes nu crud chalu karyu . get all thyu khalii bolo

--- Areas/Admin/Controllers/ShoesController.cs
+++ Areas/Admin/Controllers/ShoesController.cs
@@ -1,13 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Online_Shoes_Shop.Controllers;
+using Online_Shoes_Shop.Models;
 
 namespace Online_Shoes_Shop.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ShoesController : Controller
     {
-        public IActionResult GetAll()
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
+
+        public ShoesController(IConfiguration configuration)
         {
-            return View();
+            _configuration = configuration;
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new System.Uri(_configuration["WebApiBaseUrl"])
+            };
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _httpClient.GetAsync("api/Shoes");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                var shoes = JsonConvert.DeserializeObject<List<ShoesModel>>(data);
+                return View(shoes);
+            }
+            return View(new List<ShoesModel>());
         }
     }
 }
--- Models/ShoesModel.cs
+++ Models/ShoesModel.cs
@@ -9,7 +9,7 @@
 
         public double Price { get; set; } // Corresponds to Price
         public string ImageURL { get; set; } // Corresponds to ImageURL
-        public byte[] Image { get; set; } // Corresponds to Image (stored as binary data)
+        public IFormFile Image { get; set; } // Corresponds to Image (stored as binary data)
         public string Description { get; set; } // Corresponds to Description
         public int Stock { get; set; } // Corresponds to Stock
         public DateTime CreatedDate { get; set; } // Corresponds to CreatedDate
